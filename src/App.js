dab4aff2e349977f55073e518d0e62583d155929
mimura <musyu36@gmail.com>
2020-05-14 10:12:34 +0900

    テキスト編集機能を追加

--- src/App.js
+++ src/App.js
@@ -1,37 +1,35 @@
 import React, { Component } from "react";
 import "./App.css";
 
-// function App() {
-//   const pos = { x: 0, y: 0 };
-
-//   return (
-//     <div
-//       className="App"
-//       onDrop={(e) => setPos({ x: e.clientX, y: e.clientY })}
-//       onDragOver={(e) => e.preventDefault()}
-//     >
-//       <div className="Memo" draggable="true">
-//         テキスト入力
-//       </div>
-//     </div>
-//   );
-// }
-
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       x: 0,
       y: 0,
+      text: "テキスト入力",
+      isEditing: false,
     };
 
     this.updatePos = this.updatePos.bind(this);
+    this.updateEditing = this.updateEditing.bind(this);
+    this.updateText = this.updateText.bind(this);
   }
 
   updatePos(newPos) {
     this.setState((state) => ({
       x: newPos.x,
       y: newPos.y,
+    }));
+  }
+  updateEditing(newState) {
+    this.setState((state) => ({
+      isEditing: newState,
+    }));
+  }
+  updateText(newText) {
+    this.setState((state) => ({
+      text: newText,
     }));
   }
   render() {
@@ -50,7 +48,21 @@ class App extends Component {
           }}
           draggable="true"
         >
-          テキスト入力
+          {this.state.isEditing ? (
+            <textarea
+              name=""
+              id=""
+              cols="15"
+              rows="5"
+              onBlur={(e) => this.updateEditing(false)}
+              onChange={(e) => this.updateText(e.target.value)}
+              defaultValue={this.state.text}
+            ></textarea>
+          ) : (
+            <div onClick={(e) => this.updateEditing(true)}>
+              {this.state.text}
+            </div>
+          )}
         </div>
       </div>
     );
