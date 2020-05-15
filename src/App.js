b9537232dfc6e2a505a8d8126afa90dbe021ffa0
mimura <musyu36@gmail.com>
2020-05-15 09:13:59 +0900

    複数テキストの編集に対応

--- src/App.js
+++ src/App.js
@@ -3,15 +3,47 @@ import "./App.css";
 
 const App = () => {
   const [pos, setPos] = React.useState({ x: 0, y: 0 });
-  const [text, setText] = React.useState("テキストを入力");
-  const [editing, setEditing] = React.useState(false);
+  // const [text, setText] = React.useState("テキストを入力");
+  const [editing, setEditing] = React.useState({ key: "" });
+  const [memos, setMemos] = React.useState({
+    id1: { t: "テキスト1", x: 0, y: 0 },
+    id2: { t: "テキスト２", x: 100, y: 100 },
+  });
+  const updateMemo = (key, memo) => setMemos({ ...memos, [key]: memo });
   return (
     <div
       className="App"
       onDrop={(e) => setPos({ x: e.clientX, y: e.clientY })}
       onDragOver={(e) => e.preventDefault()}
     >
-      <div
+      {Object.keys(memos).map((key) => (
+        <div
+          key={key}
+          style={{
+            position: "absolute",
+            top: memos[key].y + "px",
+            left: memos[key].x + "px",
+          }}
+          draggable={true}
+        >
+          {editing.key === key ? (
+            <textarea
+              name=""
+              id=""
+              cols="15"
+              rows="5"
+              onBlur={(e) => setEditing({ key: "" })}
+              onChange={(e) =>
+                updateMemo(key, { ...memos[key], t: e.target.value })
+              }
+              defaultValue={memos[key].t}
+            ></textarea>
+          ) : (
+            <div onClick={(e) => setEditing({ key })}>{memos[key].t}</div>
+          )}
+        </div>
+      ))}
+      {/* <div
         className="Memo"
         style={{
           position: "absolute",
@@ -33,7 +65,7 @@ const App = () => {
         ) : (
           <div onClick={(e) => setEditing(true)}>{text}</div>
         )}
-      </div>
+      </div> */}
     </div>
   );
 };
